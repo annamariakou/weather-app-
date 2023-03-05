b9ade7304c3ac711f8b3861286a976e270cadf4c
annamariakou <110191105+annamariakou@users.noreply.github.com>
2023-03-05 11:37:38 +0000

    update apiKey

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ function formatDay (timestamp) {
 // Forecast 
 
 function displayForecast(response) {
+  console.log(response);
   let forecast = response.data.daily;
 
   let forecastElement = document.querySelector("#forecast");
@@ -78,8 +79,8 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "0f8bc384a7c31b717a18cfe38a95ae06";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiKey = "5201594abea9f3e38b70e65b11a80c24";
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.latitude}}&lon=${coordinates.longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -109,14 +110,14 @@ function currentWeather(response) {
 
   // api forecast 
 
-  getForecast(response.data.coord);
+  getForecast(response.data.coordinates);
   
 }
 
 // city
 
 function search(city) {
-  let apiKey = "0f8bc384a7c31b717a18cfe38a95ae06";
+  let apiKey = "5201594abea9f3e38b70e65b11a80c24";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(currentWeather);
 }
@@ -128,7 +129,7 @@ function submit(event) {
 }
 
 function searchLocation(position) {
-  let apiKey = "0f8bc384a7c31b717a18cfe38a95ae06";
+  let apiKey = "5201594abea9f3e38b70e65b11a80c24";
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
